9e9159c471af54bd4a7deb675a8632d4d76d915f
Wisdom Chionye <officialsabimywork@gmail.com>
2025-08-13 13:17:37 +0000

    stms logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
       <Image 
-        src="/logo.jpg" 
-        alt="STMS OWN Logo" 
+        src="/stms-logo.jpg" 
+        alt="STMS Logo" 
         width={1200} 
         height={400}
         className="w-auto h-48 sm:h-64 md:h-80 lg:h-96"
