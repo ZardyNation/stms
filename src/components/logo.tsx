fb9947652ef4ea7882796a5cb412b8d6d8335d23
Wisdom Chionye <officialsabimywork@gmail.com>
2025-08-14 16:15:59 +0000

    Remove the logo there, Add IA Awards for Now, we will send logo later, C

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,11 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <Image 
-        src="/stms%20logo.jpg" 
-        alt="STMS Logo" 
-        width={1200} 
-        height={400}
-        className="w-auto h-48 sm:h-64 md:h-80 lg:h-96"
-      />
+      <span className="text-5xl font-extrabold tracking-tight text-primary">
+        IA Awards
+      </span>
     </Link>
   );
 }
